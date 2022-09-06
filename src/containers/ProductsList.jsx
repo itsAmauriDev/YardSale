d96506fd7fa24c49ac4ef10b7b95c8b58dc07edb
Jorge Guillen <jguillenm1500@alumno.ipn.mx>
2022-09-06 18:26:06 -0500

    Add use Effect to get data from API

--- src/containers/ProductsList.jsx
+++ src/containers/ProductsList.jsx
@@ -1,31 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ProductItem from "@components/ProductItem";
 import "@styles/ProductsList.scss";
 
-import ProductItem from "@components/ProductItem";
+const API = "https://api.escuelajs.co/api/v1/products";
 
 const ProductsList = () => {
+  const [products, setProducts] = useState([]);
+
+  const getData = async () => {
+    const response = await axios(API);
+    setProducts(response.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <section className='main-container'>
       <div className='products-container'>
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
+        {products.map((product) => (
+          <ProductItem />
+        ))}
       </div>
     </section>
   );
